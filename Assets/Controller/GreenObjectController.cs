39479fdd16186efeda071c718e8980da5c77e9e2
samsonllam <fongpuilam@gmail.com>
2020-01-30 05:15:08 +0800

    init

---
+++ Assets/Controller/GreenObjectController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class GreenObjectController : MonoBehaviour
+{
+    public Camera camera;
+    public NavMeshAgent agent;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit)){
+                // Move agent
+                agent.SetDestination(hit.point);
+            }       
+        }
+    }
+}
---
+++ Tutorial Defaults/Controller/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera camera;
+    public NavMeshAgent agent;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Debug.Log("Mouse Clicked");
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Debug.Log(Physics.Raycast(ray, out hit));
+            if(Physics.Raycast(ray, out hit)){
+                // Move agent
+                Debug.Log("Move");
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
